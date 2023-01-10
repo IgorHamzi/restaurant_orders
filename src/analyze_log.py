13537a5cfcf67fd64483d6befc5c299c28c15741
IgorHamzi <igorwhamzi@gmail.com>
2023-01-10 12:49:50 -0300

    refactor: importer_csv function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -6,30 +6,34 @@ def importer_csv(path_to_file):
         raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
 
     try:
-        requests = dict()
         with open(path_to_file) as file:
-            for i in csv.reader(file):
-                name, food, day = i[0], i[1], i[2]
-                if name not in requests.keys():
-                    requests[name] = {food: {day: 1}}
-                elif food not in requests[name].keys():
-                    requests[name][food] = {day: 1}
-                elif day not in requests[name][food].keys():
-                    requests[name][food][day] = 1
-                else:
-                    requests[name][food][day] += 1
-
-        return requests
+            return format_file(file)
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
 
+
+def format_file(file):
+    requests = dict()
+
+    for i in csv.reader(file):
+        name, food, day = i[0], i[1], i[2]
+        if name not in requests.keys():
+            requests[name] = {food: {day: 1}}
+        elif food not in requests[name].keys():
+            requests[name][food] = {day: 1}
+        elif day not in requests[name][food].keys():
+            requests[name][food][day] = 1
+        else:
+            requests[name][food][day] += 1
+
+    return requests
 
 
 def most_requested_dish_by_maria(path_to_file):
     requests = importer_csv(path_to_file)
 
     frequency = {}
-    
+
     for i in requests['maria'].keys():
         for j in requests['maria'][i]:
             frequency[i] = requests['maria'][i][j]
@@ -82,17 +86,16 @@ def days_off_joao(path_to_file):
     return all_days - days_joao
 
 
-# def analyze_log(path_to_file):
-#     a = most_requested_dish_by_maria(path_to_file)
-#     b = how_many_hamburgers_did_arnaldo_order(path_to_file)
-#     c = how_many_dishes_did_joao_ever_order(path_to_file)
-#     d = days_off_joao(path_to_file)
-# 
-#     resposta = f'{a}\n{b}\n{c}\n{d}'
-#     with open('data/mkt_campaign.txt', 'w') as f:
-#         f.write(resposta)
-# 
-# if __name__ == '__main__':
-#     print(analyze_log('data/orders_1.csv'))
-# 
-# 
+def analyze_log(path_to_file):
+    a = most_requested_dish_by_maria(path_to_file)
+    b = how_many_hamburgers_did_arnaldo_order(path_to_file)
+    c = how_many_dishes_did_joao_ever_order(path_to_file)
+    d = days_off_joao(path_to_file)
+
+    resposta = f'{a}\n{b}\n{c}\n{d}'
+    with open('data/mkt_campaign.txt', 'w') as f:
+        f.write(resposta)
+
+
+if __name__ == '__main__':
+    print(analyze_log('data/orders_1.csv'))
