3f8a91f3cb421adce2053b97fe4fb0e4e7541d74
IgorHamzi <igorwhamzi@gmail.com>
2023-01-10 14:27:06 -0300

    feat: add never ordered per customer function

--- src/track_orders.py
+++ src/track_orders.py
@@ -26,10 +26,26 @@ class TrackOrders:
         return max(frequency, key=frequency.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_dishes = set()
+        dishes_customer = set()
 
-    def get_days_never_visited_per_customer(self, customer):
-        pass
+        for i in self.orders:
+            all_dishes.add(i[1])
+            if i[0] == customer:
+                dishes_customer.add(i[1])
+
+        return all_dishes - dishes_customer
+
+#    def get_days_never_visited_per_customer(self, customer):
+#        all_days = set()
+#        days_customer = set()
+#
+#        for i in self.orders:
+#            all_days.add(i[2])
+#            if i[0] == customer:
+#                days_customer.add(i[2])
+#
+#        return all_days - days_customer
 
     def get_busiest_day(self):
         pass
@@ -37,19 +53,20 @@ class TrackOrders:
     def get_least_busy_day(self):
         pass
 
-# if __name__ == '__main__':
-#     csv_parsed = [
-#     ["maria", "pizza", "terça-feira"],
-#     ["maria", "hamburguer", "terça-feira"],
-#     ["joao", "hamburguer", "terça-feira"],
-#     ["maria", "coxinha", "segunda-feira"],
-#     ["arnaldo", "misto-quente", "terça-feira"],
-#     ["jose", "hamburguer", "sabado"],
-#     ["maria", "hamburguer", "terça-feira"],
-#     ["maria", "hamburguer", "terça-feira"],
-#     ["joao", "hamburguer", "terça-feira"],
-# ]
-#     track_orders = TrackOrders()
-#     for name, food, day in csv_parsed:
-#         track_orders.add_new_order(name, food, day)
-#     print(track_orders.get_most_ordered_dish_per_customer('maria'))
+
+if __name__ == '__main__':
+    csv_parsed = [
+    ["maria", "pizza", "terça-feira"],
+    ["maria", "hamburguer", "terça-feira"],
+    ["joao", "hamburguer", "terça-feira"],
+    ["maria", "coxinha", "segunda-feira"],
+    ["arnaldo", "misto-quente", "terça-feira"],
+    ["jose", "hamburguer", "sabado"],
+    ["maria", "hamburguer", "terça-feira"],
+    ["maria", "hamburguer", "terça-feira"],
+    ["joao", "hamburguer", "terça-feira"],
+]
+    track_orders = TrackOrders()
+    for name, food, day in csv_parsed:
+        track_orders.add_new_order(name, food, day)
+    print(track_orders.get_never_ordered_per_customer('maria'))
